6493022f6a888f6e16582cac46955979441f4feb
Sai Aung Kyaw Maw <saiaungkyawmaw@gmail.com>
2023-10-09 11:11:52 -0400

    userDetail component file uploaded
    Haven't done css yet

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {
-  Typography
+    List, Typography, Button, ListItemText, ListItem, Divider
 } from '@mui/material';
 import './userDetail.css';
+import {Link} from "react-router-dom";
 
 
 /**
@@ -11,17 +12,73 @@ import './userDetail.css';
 class UserDetail extends React.Component {
   constructor(props) {
     super(props);
+
+      this.state = { user : null };
   }
 
-  render() {
+    componentDidMount(){
+      const userId = this.props.match.params.userId;
+
+      console.log("UserId:", userId);
+
+      const userDetails = window.models.userModel(userId);
+
+      console.log("User Details:", userDetails);
+
+      this.setState({user : userDetails});
+    }
+
+    componentDidUpdate(prevProps) {
+        const currentUserId = this.props.match.params.userId;
+        const prevUserId = prevProps.match.params.userId;
+
+        console.log("Previous Id: " , prevUserId);
+        console.log("Current Id: ", currentUserId);
+
+        if (currentUserId !== prevUserId) {
+            const userDetails = window.models.userModel(currentUserId);
+            this.setState({ user: userDetails });
+            console.log("User Details: ", userDetails);
+        }
+
+    }
+
+    render() {
+        const { user } = this.state;
+
+        if(!user) return <Typography variant = "body1"> Loading...</Typography>;
     return (
-      <Typography variant="body1">
-        This should be the UserDetail view of the PhotoShare app. Since
-        it is invoked from React Router the params from the route will be
-        in property match. So this should show details of user:
-        {this.props.match.params.userId}. You can fetch the model for the
-        user from window.models.userModel(userId).
-      </Typography>
+        <div className="userDetail">
+            <Typography variant="h4">{`${user.first_name} ${user.last_name}`}</Typography>
+
+            <List component="nav">
+                <ListItem>
+                    <ListItemText primary="First Name" secondary={user.first_name} />
+                </ListItem>
+                <Divider />
+                <ListItem>
+                    <ListItemText primary="Last Name" secondary={user.last_name} />
+                </ListItem>
+                <Divider />
+                <ListItem>
+                    <ListItemText primary="Location" secondary={user.location} />
+                </ListItem>
+                <Divider />
+                <ListItem>
+                    <ListItemText primary="Description" secondary={user.description} />
+                </ListItem>
+                <Divider />
+                <ListItem>
+                    <ListItemText primary="Occupation" secondary={user.occupation} />
+                </ListItem>
+            </List>
+
+            <Link to={`/photos/${user._id}`}>
+                <Button variant="contained" color="primary">
+                    View Photos
+                </Button>
+            </Link>
+        </div>
     );
   }
 }
