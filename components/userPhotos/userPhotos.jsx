3857112557ad83ce9bd0bb510997dc1827626bab
Sai Aung Kyaw Maw <saiaungkyawmaw@gmail.com>
2023-10-12 12:54:05 -0400

    added check for comment (array or not)

--- components/userPhotos/userPhotos.jsx
+++ components/userPhotos/userPhotos.jsx
@@ -12,10 +12,17 @@ class UserPhotos extends React.Component {
     const userId = this.props.match.params.userId;
     const userPhotos = window.models.photoOfUserModel(userId);
     this.setState({ userPhotos } );
+    console.log()
   }
 
   render() {
     const { userPhotos } = this.state;
+
+    if (Array.isArray(this.state.userPhotos)) {
+      console.log('userPhotos is an array!', this.state.userPhotos);
+    } else {
+      console.log('userPhotos is NOT an array. Its value is:', this.state.userPhotos);
+    }
 
     if (!userPhotos || userPhotos.length === 0) {
       return <Typography variant="body1">Loading...</Typography>;
@@ -28,18 +35,18 @@ class UserPhotos extends React.Component {
                 <img src={`../images/${photo.file_name}`} alt="User's post" />
                 <Typography variant="body1">{photo.date_time}</Typography>
                 <List component="nav">
-                  {photo.comments.map(comment => (
+                  {Array.isArray(photo.comments) && photo.comments.map(comment => (
                       <ListItem key={comment._id}>
                         <ListItemText
                             primary={comment.comment}
-                            secondary={
+                            secondary={(
                               <React.Fragment>
                                 <Link to={`/users/${comment.user._id}`}>
                                   {`${comment.user.first_name} ${comment.user.last_name}`}
                                 </Link>
                                 {` - ${comment.date_time}`}
                               </React.Fragment>
-                            }
+                            )}
                         />
                       </ListItem>
                   ))}
